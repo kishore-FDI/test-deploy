59fa9d4ffd181dfd76765a7660b00d3f51d8f8b9
Kishore <kishore22705@gmail.com>
2024-05-27 19:08:34 +0530

    first commit

---
+++ venv/main.py
@@ -0,0 +1,20 @@
+from flask import Flask,jsonify
+from flask_cors import CORS
+
+
+app=Flask(__name__)
+cors=CORS(app,orgins='*')
+
+@app.route("/api/users/",methods=["GET"])
+def users():
+    a=1+2
+    return jsonify(
+        {
+            "users":[
+                a
+            ]
+        }
+    )
+
+if __name__=="__main__":
+    app.run(debug=True,port=8080)
